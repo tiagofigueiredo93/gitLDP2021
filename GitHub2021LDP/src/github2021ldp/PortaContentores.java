cbe004bf418aefe2542c86628cfaea2ed9f20534
tiagofigueiredo93 <tiago.figueiredo.bcl@gmail.com>
2021-03-13 19:31:39 +0000

    Commit desenvolvido pelo ProgramadorC (PortaContentores)

---
+++ GitHub2021LDP/src/github2021ldp/PortaContentores.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github2021ldp;
+
+/**
+ *
+ * @author tiagofigueiredo
+ */
+public class PortaContentores extends Navio {
+    private int maxContentores;
+    
+    //Contrutor
+    
+    public PortaContentores(String Matricula){
+        super(Matricula);
+        maxContentores = 0;
+    }
+       
+    //Getter
+    public int getMaxContentores() {
+        return maxContentores;
+    }
+    
+    //Setter
+    public void setMaxContentores(int maxContentores) {
+        this.maxContentores = maxContentores;
+    }
+
+    @Override
+    public String toString() {
+        return "PortaContentores{" + "maxContentores=" + maxContentores + '}';
+    }
+    
+    
+    
+    
+    
+}
